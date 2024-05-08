878676c0b325754b5dd35fb52ea31f17299031a7
Tamal Saha <tamal@appscode.com>
2024-05-08 18:51:30 +0600

    Add capi-ops-manager chart
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/register.go
+++ apis/installer/v1alpha1/register.go
@@ -56,6 +56,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
 		&CapaVpcPeeringOperator{},
 		&CapaVpcPeeringOperatorList{},
+		&CapiOpsManager{},
+		&CapiOpsManagerList{},
 		&DockerMachineOperator{},
 		&DockerMachineOperatorList{},
 	)
