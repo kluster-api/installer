5285a713f0e121bb563e7d33bc13c771ee5e8f36
Tamal Saha <tamal@appscode.com>
2025-03-14 09:28:35 -0400

    Add irsa mode
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/aws_credential_manager_types.go
+++ apis/installer/v1alpha1/aws_credential_manager_types.go
@@ -82,6 +82,7 @@ type AwsCredentialManagerSpec struct {
 	ServiceAccount ServiceAccountSpec `json:"serviceAccount"`
 	Volumes        []core.Volume      `json:"volumes"`
 	VolumeMounts   []core.VolumeMount `json:"volumeMounts"`
+	Args           []string           `json:"args"`
 }
 
 type ImageReference struct {
